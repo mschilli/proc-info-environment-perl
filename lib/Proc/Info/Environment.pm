b7d43989b416eeaabf412bed2db1e86276dbc478
mschilli <github@perlmeister.com>
2010-06-26 09:20:56 -0700

    fixed docs

--- lib/Proc/Info/Environment.pm
+++ lib/Proc/Info/Environment.pm
@@ -69,19 +69,52 @@ __END__
 
 =head1 NAME
 
-Proc::Info::Environment - blah blah blah
+Proc::Info::Environment - Read Environment Variables of any Process
 
 =head1 SYNOPSIS
 
     use Proc::Info::Environment;
+    my $proc_info = Proc::Info::Environment->new();
+
+    my $env = $proc_info->env( $pid );
+
+    print "\$PATH of process $pid is $env->{PATH}\n";
 
 =head1 DESCRIPTION
 
-Proc::Info::Environment blah blah blah.
+Proc::Info::Environment retrieves the settings of environment variables
+in arbitrary running processes of the operating system.
 
-=head1 EXAMPLES
+For example,
 
-  $ perl -MProc::Info::Environment -le 'print $foo'
+    use Proc::Info::Environment;
+    my $proc_env = Proc::Info::Environment->new();
+    my $env = $proc_env->env( 123 );
+
+will retrieve the environment settings of the process with the pid 123 
+and (if this process exists and is readable by the user) return them as a 
+hash reference in $env. 
+
+As an illustrative (but not very useful) example, retrieving
+
+    my $env = $proc_env->env( $$ );
+
+will return a reference to a hash similar to C<%ENV>, which contains the
+environment variables of the current process.
+
+Reading other processes' environment variables is only possible 
+if the operating system (a) supports a method to do this and (b)
+the user of the currently running process is either root, or has the 
+same uid as the process whose variables it wants to read.
+
+Proc::Info::Environment is currently supported only for specific Unix
+systems. On Linux, it will use the procfs file system mounted under /proc
+to get the information needed. It will read the file /proc/xxx/environ, 
+where xxx is the pid of the target process.
+
+The module's architecture allows for easy support of other systems, if
+you want support for one and know how the information is provided,
+drop me an email.
 
 =head1 LEGALESE
 
